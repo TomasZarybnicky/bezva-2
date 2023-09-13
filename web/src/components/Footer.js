a30a70d2f84c45ff4ef605b48f5798ddbeaa76ec
Tomáš Zárybnický <tomzaryb@seznam.cz>
2023-09-13 21:47:26 +0200

    1.5 (13.9)

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -1,8 +1,9 @@
 import "./Footer.css"
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
+  return ( <div>
+      <p>Footer</p>  
+    </div>
   )
 }
 
--- web/src/pages/AllKempy.js
+++ web/src/pages/AllKempy.js
@@ -1,6 +1,32 @@
+import { projectFirestore } from "../firebase/config"
+import { useState, useEffect } from "react"
+import { link } from "react-router-dom"
+
 const AllKempy = () => {
+  const [data, setData] = useState([])
+  const [error, setError] = useState("")
+
+  useEffect( () => {
+    projectFirestore.collection("kempy").get().then( (snapshot) => {
+
+      if (snapshot.empty) {
+        setError("Žádné kempy k vypsání")
+      } else {
+        let result = []
+        snapshot.docs.forEach( (oneKemp) => {
+          result.push( {id: oneKemp.id, ...oneKemp.data()} )
+        })
+        setData(result)
+      }
+    }).catch( (err) => {
+      setError(err.message)
+    })
+  }, [])
+
   return (
-    <div>AllKempy</div>
+    <section>
+      {error && <p>{error}</p>}
+    </section>
   )
 }
 
