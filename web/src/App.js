32e2dd12c58dc64f1a982772ee96c8c3030fadfd
Tomáš Zárybnický <tomzaryb@seznam.cz>
2023-09-15 19:51:36 +0200

    2. přidání TailWind

--- web/src/App.js
+++ web/src/App.js
@@ -4,6 +4,7 @@ import AllKempy from "./pages/AllKempy"
 import OneKemp from "./pages/OneKemp"
 import Form from "./pages/Form"
 import SharedLayout from "./pages/SharedLayout"
+import FormDelete from "./pages/FormDelete"
 
 const App = () => {
   return <BrowserRouter>
@@ -13,6 +14,7 @@ const App = () => {
         <Route index element={ <Home /> } />
         <Route path="all-kempy" element={ <AllKempy /> } />
         <Route path="form" element={ <Form /> } />
+        <Route path="form-delete" element={ <FormDelete /> } />
         <Route path="one-kemp/:movieId" element={ <OneKemp /> } />
       </Route>
 
